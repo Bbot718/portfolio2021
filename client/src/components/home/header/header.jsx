003a5bdeb1879e2ea66a50be912af7e15dfa9f4f
BenBotros <helo@benjaminbotro.ch>
2021-07-07 09:55:49 +0200

    Experimenting Header In

--- client/src/components/home/header/header.jsx
+++ client/src/components/home/header/header.jsx
@@ -1,22 +1,38 @@
 import React, {useEffect} from 'react';
 
+import gsap, {Expo} from 'gsap';
+import SplitText from 'gsap/SplitText'
+
 const Header = (props) => {
+
+  useEffect(() => {
+    const tl = gsap.timeline();
+    
+    const splitText = new SplitText(".header__heading", {type:"lines, words, chars"});
+    const lines = splitText.lines;
+    const words = splitText.words;
+    const chars = splitText.chars;
+
+    //gsap.from(words, {y: '100%', stagger: 0.05, ease: Expo.easeInOut, duration:1.5});
+    gsap.from(chars, {rotateX: '200%', stagger: 0.05, ease: Expo.easeInOut, duration:1.5});
+
+  }, []);
 
 
   return(
     <section className="header">
       <div className="header__content">
-        <div className="tertiary-heading header__heading">Hello Friend,</div>
+        <div className="wrapper"><div className="tertiary-heading header__heading">Hello Friend,</div></div>
         <hr className="header__line line-large" />
-        <div className="tertiary-heading header__heading">I'm Ben, a Passionate</div>
+        <div className="wrapper"><div className="tertiary-heading header__heading">I'm Ben, a Passionate</div></div>
         <hr className="header__line line-large" />
-        <div className="tertiary-heading tertiary-heading--bold header__heading header__heading--offset">Digital</div>
+        <div className="wrapper"><div className="tertiary-heading tertiary-heading--bold header__heading header__heading--offset">Digital</div></div>
         <hr className="header__line line-large" />
-        <div className="tertiary-heading tertiary-heading--bold header__heading header__heading--offset">Designer</div>
+        <div className="wrapper"><div className="tertiary-heading tertiary-heading--bold header__heading header__heading--offset">Designer</div></div>
         <hr className="header__line line-large" />
-        <div className="tertiary-heading header__heading">Based In Lausanne</div>
+        <div className="wrapper"><div className="tertiary-heading header__heading">Based In Lausanne</div></div>
         <hr className="header__line line-large" />
-        <div className="tertiary-heading header__heading">Switzerland</div>
+        <div className="wrapper"><div className="tertiary-heading header__heading">Switzerland</div></div>
       </div>
     </section>
   )
